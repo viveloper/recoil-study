f0cc2faf3bbc49d95052761ffdabdc3c70cfdd94
viveloper <ucsik1234@gmail.com>
2023-04-19 23:12:40 +0900

    count list : using atom and selectorFamily

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
 import Button from '@mui/material/Button';
-import { useRecoilState } from 'recoil';
-import { countListState } from './store';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { countListState, countState } from './store';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import Grid from '@mui/material/Grid';
+import { useCallback } from 'react';
 
 function App() {
-  const [countList, setCountList] = useRecoilState(countListState);
   return (
     <div className="App">
       <Grid container spacing={2}>
         {[0, 1, 2].map((id) => (
           <Grid item xs={4} key={id}>
-            <CountCard
-              count={countList[id]}
-              onClick={() =>
-                setCountList((prev) =>
-                  prev.map((count, i) => (id === i ? count + 1 : count))
-                )
-              }
-            />
+            <CountCardContainer id={id} />
           </Grid>
         ))}
       </Grid>
@@ -30,6 +23,20 @@ function App() {
 }
 
 export default App;
+
+function CountCardContainer({ id }: { id: number }) {
+  const count = useRecoilValue(countState(id));
+  const setCountList = useSetRecoilState(countListState);
+
+  const handleClick = useCallback(
+    () =>
+      setCountList((prev) =>
+        prev.map((count, i) => (id === i ? count + 1 : count))
+      ),
+    []
+  );
+  return <CountCard count={count} onClick={handleClick} />;
+}
 
 function CountCard({ count, onClick }: { count: number; onClick: () => void }) {
   return (
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,25 @@
-import { atom } from 'recoil';
+import { atom, selectorFamily } from 'recoil';
 
 export const countListState = atom({
   key: 'countListState',
   default: [0, 0, 0],
+});
+
+export const countState = selectorFamily<number, number>({
+  key: 'countState',
+  get:
+    (id) =>
+    ({ get }) => {
+      return get(countListState)[id];
+    },
+
+  // optional set
+  // set:
+  //   (id) =>
+  //   ({ set, get }, newValue) => {
+  //     set(
+  //       countListState,
+  //       get(countListState).map((count, i) => (id === i ? count + 1 : count))
+  //     );
+  //   },
 });
